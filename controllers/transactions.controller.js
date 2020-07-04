c259c4123d792bed2204009e04931cb247f868b7
v4n8877 <vinh.ngoanh@gmail.com>
2020-07-04 22:31:15 +0700

    Finish bai 9-1

---
+++ controllers/transactions.controller.js
@@ -0,0 +1,47 @@
+const shortid = require('shortid');
+const db = require('../db');
+
+module.exports.index = (req, res) => {
+  res.render('transactions', {
+    trans: db.get('trans').value()
+  });
+};
+
+module.exports.createTrans = (req, res) => {
+  res.render('createTrans', {
+    users: db.get('users').value(),
+    books: db.get('books').value(),
+  });
+};
+
+module.exports.transCreated = (req, res) => {
+  req.body.id = shortid.generate();
+  db.get('trans').push(req.body).write();
+  res.redirect('/trans');
+};
+
+module.exports.idUpdate = (req, res) => {
+  const transId = req.params.transId;
+
+  res.render('updateTrans', {
+    users: db.get('users').value(),
+    books: db.get('books').value(),
+    transId: transId
+  });
+};
+
+module.exports.transUpdated = (req, res) => {
+  console.log(req.body);
+  db.get('trans')
+  .find({ id: req.body.id })
+  .assign({ userId: req.body.userId, bookId: req.body.bookId})
+  .write();
+  
+  res.redirect('/trans');
+};
+
+module.exports.deleteTrans = (req, res) => {
+  const transId = req.params.transId;
+  db.get('trans').remove({ id: transId }).write();
+  res.redirect('/trans');
+};
