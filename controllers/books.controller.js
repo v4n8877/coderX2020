6e7bb9d4e663045392d6d2c26f8cd19b8182722f
v4n8877 <vinh.ngoanh@gmail.com>
2020-07-27 20:39:37 +0700

    Complete express-20

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,5 +1,6 @@
 const shortid = require('shortid');
 const db = require('../db');
+const makePagination = require('../midlewares/pagination.midleware');
 
 module.exports.index = (req, res) => {
   const page = parseInt(req.query.page) || 1;
@@ -8,10 +9,11 @@ module.exports.index = (req, res) => {
   const endPage = page * perPage;
   const pageCount = Math.ceil(db.get('books').value().length / perPage);
   const convertPageCount = Array.from(Array(pageCount), (_, i) => i + 1);
-
+  const newPagination = makePagination.customPagination(page, pageCount);
+  console.log("a", newPagination)
   res.render('books', {
     books: db.get('books').value().slice(startPage, endPage),
-    pageCount: convertPageCount,
+    pageCount: newPagination,
     currentPage: page
   });
 };
