22f051393bd2d0f6d4d2f899715c740365459e90
v4n8877 <vinh.ngoanh@gmail.com>
2020-08-11 11:26:29 +0700

    Change port for app

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const authMidleware = require('./midlewares/auth.midleware');
 const sessionMidleware = require('./midlewares/session.midleware');
 
 const app = express();
-const port = 3000;
+const port = process.evn.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views','./views');
